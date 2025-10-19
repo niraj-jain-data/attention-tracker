3be4662719d1d104b36bb3b7e7f85c38ef464cde
Niraj Jain <njain2698.nj@gmail.com>
2025-10-19 16:21:07 +0100

    Ninth Commit

--- static/script.js
+++ static/script.js
@@ -1,20 +1,15 @@
-// Works for both desktop clicks and mobile touches
 function sendClick(x, y) {
   fetch("/api/click", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ x, y })
   })
-  .then(res => {
-    if (!res.ok) throw new Error("Network error");
-    return res.json();
-  })
-  .then(data => console.log("Saved:", data))
-  .catch(err => console.error("Error saving click:", err));
+  .then(res => res.json())
+  .then(data => console.log("Tap saved:", data))
+  .catch(err => console.error("Error:", err));
 }
 
 function getCoordinates(e) {
-  // Support both mouse and touch
   let x, y;
   if (e.touches && e.touches.length > 0) {
     const touch = e.touches[0];
@@ -27,6 +22,5 @@ function getCoordinates(e) {
   sendClick(x, y);
 }
 
-// Listen for both clicks and touches
 document.addEventListener("click", getCoordinates);
 document.addEventListener("touchend", getCoordinates);
